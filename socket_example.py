2eca6b4f57d184b007d4c3d114b7be967384d951
Захаров Павел <pzakharov83@gmail.com>
2022-03-16 20:40:40 +0300

    add proper response to websocket protocol

--- socket_example.py
+++ socket_example.py
@@ -1,14 +1,25 @@
+import json
 from contextlib import suppress
 
 import trio
 from trio_websocket import ConnectionClosed, serve_websocket
 
 
+MESSAGE = {
+  "msgType": "Buses",
+  "buses": [
+    {"busId": "c790сс", "lat": 55.7500, "lng": 37.600, "route": "120"},
+    {"busId": "a134aa", "lat": 55.7494, "lng": 37.621, "route": "670к"},
+  ]
+}
+
+
 async def echo_server(request):
     ws = await request.accept()
     while True:
         try:
-            message = await ws.get_message()
+            # message = await ws.get_message()
+            message = json.dumps(MESSAGE)
             await ws.send_message(message)
         except ConnectionClosed:
             break
